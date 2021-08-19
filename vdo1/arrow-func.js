09a86b1665ca8c3c53c2232d7e460854cc850975
Sahadat Hossen <sahadatjh@gmail.com>
2021-08-19 11:45:55 +0600

    First commit

---
+++ vdo1/arrow-func.js
@@ -0,0 +1,16 @@
+// function number() {
+//     return 10;
+// }
+
+// console.log(number());
+
+//--------------
+function addition(a, b) {
+    return a + b;
+}
+
+console.log(addition(10, 5));
+
+// arrow function
+let summation = (a, b) => a + b;
+console.log(summation(20, 5));
